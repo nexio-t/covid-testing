7ee75ef82569864320e635b50d44e322e41b896f
Tomas <tomas.e.gear@gmail.com>
2021-01-07 00:30:35 -0500

    modify styling of victory chart to be smaller, cleaner

--- src/components/BarChartRace.js
+++ src/components/BarChartRace.js
@@ -1,26 +1,32 @@
 import React, { useState, useEffect } from 'react';
 // import React from 'react';
 import ReactDOM from 'react-dom';
-import { VictoryChart, VictoryBar, Bar } from 'victory';
+import { VictoryAxis, VictoryTheme, VictoryChart, VictoryBar, Bar } from 'victory';
 
 function BarChartRace({ data }) {
 
     // will be called initially and on every data change
 
-    const [style, setStyle] = useState({ data: { fill: "tomato" } });
+    const [style, setStyle] = useState({ data: { fill: "tomato", fontSize: 4 }, labels: {
+      fontSize: 4,
+    }  });
     const [clicked, setClicked] = useState(false);
 
 
     const handleMouseOver = () => {
         
-        console.log("handleMouseOver called"); 
-        console.log("clicke is: ", clicked); 
+        // console.log("handleMouseOver called"); 
+        // console.log("clicke is: ", clicked); 
 
-        
+        // console.log("isClicked", isClicked); 
 
-        console.log("isClicked", isClicked); 
+        const isClicked = !clicked;
+        console.log("isClicked2 is: ", isClicked); 
+        setClicked(isClicked);
+        const fillColor = clicked ? "blue" : "tomato";
+        console.log("fillColor is: ", fillColor); 
+        setStyle({data: { fill: fillColor }})
 
-       
        
         // this.setState({
         //   clicked,
@@ -37,29 +43,43 @@ function BarChartRace({ data }) {
         // Begin incorporating data (see how it looks)
         // Change size of graph 
         // Then add the other graphs 
+        // https://github.com/FormidableLabs/victory-tutorial/blob/9bf170061599027e4bd5fcf8128e47adb83c0e98/src/js/client.js
+        // https://formidable.com/open-source/victory/docs/
 
     useEffect(() => {
         
-    
-        const isClicked = !clicked;
-        setClicked(isClicked);
-        setStyle({data: { fill: fillColor }})
-        const fillColor = clicked ? "blue" : "tomato";
-
-
-    }, [clicked, data]);
+ 
+    }, [data]);
   
     return (
        <div>
-        <VictoryChart height={400} width={400}
-          domainPadding={{ x: 50, y: [0, 20] }}
+        <VictoryChart height={150} width={150}
+          theme={VictoryTheme.material}
+          domainPadding={{ x: 10, y: [0, 10] }}
           scale={{ x: "time" }}
         >
+          <VictoryAxis
+            dependentAxis={true}
+            style={{
+              grid: { stroke: "grey" }, 
+              tickLabels: {fontSize: 3, padding: 5},
+              tickCount: 10
+            }}
+            
+          />
+          <VictoryAxis 
+            // tickFormat={(x) => ``}
+            style={{
+              grid: { stroke: "grey" },
+              tickLabels: {fontSize: 3, padding: 5}
+            }}
+          />
           <VictoryBar
             dataComponent={
               <Bar events={{ onMouseOver: handleMouseOver }}/>
             }
             style={style}
+            
             data={[
               { x: new Date(1986, 1, 1), y: 2 },
               { x: new Date(1996, 1, 1), y: 3 },
