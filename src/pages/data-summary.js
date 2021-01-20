0ec9d639bf7e32ca8bc80557e948199975687366
Tomas <tomas.e.gear@gmail.com>
2021-01-20 07:54:10 -0500

    clean up data summary page, remove logs

--- src/pages/data-summary.js
+++ src/pages/data-summary.js
@@ -1,219 +1,177 @@
 /* eslint-disable */
-import React, { useState, useEffect } from 'react';
-import Layout from 'components/Layout';
-import Container from 'components/Container';
-// var dayjs = require('dayjs')
-import dayjs from 'dayjs'
-import ChartContainer from 'components/ChartContainer'; 
-// import 'assets/stylesheets/components/_chartcontainer.scss';
+import React, { useState, useEffect } from "react";
+import Layout from "components/Layout";
+import dayjs from "dayjs";
+import ChartContainer from "components/ChartContainer";
 import _ from "lodash";
-import DataCard from '../components/DataCard'; 
-import BarChartRace from "../components/BarChartRace";
+import DataCard from "../components/DataCard";
 import BarChart from "../components/BarChart";
 import axios from "axios";
-// s
-
-// import Container from 'components/Container';
-
-
-
 
 const formatData = (data) => {
-  console.log("formatData fn data is: ", data); 
-  console.log("formatData.length is: ", data.length); 
 
-  if (data.length !== 6 || data.length === 0 || data.length === undefined) return; 
+  if (data.length !== 6 || data.length === 0 || data.length === undefined)
+    return;
 
-  // loop over that data here please and then commit \
+  return _.map(data, (day) => {
+    const {
+      deathIncrease,
+      positiveIncrease,
+      hospitalizedIncrease,
+      dateChecked,
+    } = day;
 
-  return _.map(data, day => {
-
-    const {deathIncrease, positiveIncrease, hospitalizedIncrease, dateChecked } = day; 
-
-    const abridgedDate = dayjs(`${dateChecked}`).format('MMM D'); 
-    console.log("abridgeDate is: ", abridgedDate); 
+    const abridgedDate = dayjs(`${dateChecked}`).format("MMM D");
 
     return {
       deathIncrease,
-      positiveIncrease, 
-      hospitalizedIncrease, 
-      abridgedDate
-    }
+      positiveIncrease,
+      hospitalizedIncrease,
+      abridgedDate,
+    };
+  });
+};
 
-  })
-
-}
-
-// Custom Hook To Fetch U.S. Daily Totals
 const useFetchUSDailyTotals = () => {
 
-  // const [usaCurrentData, setUsaCurrentData] = useState({ dailyTotals: [] });
-  // const [stateHistoricalData, setStateHistoricalData] = useState({stateDailyTotals: []});
-  // const [stateCurrentData, setStateCurrentData] = useState({stateDailyTotals: []});
   const [usaHistoricData, setUsaHistoricData] = useState([]);
   const [usaCurrentData, setUsaCurrentData] = useState([]);
-
-
-  const [usaHistoricUrl, setUsaHistoricUrl] = useState('https://api.covidtracking.com/v1/us/daily.json');
-  // const [stateHistoricUrl, setStateHistoricUrl] = useState('https://api.covidtracking.com/v1/states/daily.json'); 
-  // const [stateCurrentUrl, setStateCurrentUrl] = useState('https://api.covidtracking.com/v1/states/current.json'); 
-
+  const [usaHistoricUrl, setUsaHistoricUrl] = useState(
+    "https://api.covidtracking.com/v1/us/daily.json"
+  );
   const [isLoading, setIsLoading] = useState(true);
   const [isError, setIsError] = useState(false);
- 
+
   useEffect(() => {
 
     const fetchData = async () => {
+
       try {
+
         setIsError(false);
         setIsLoading(true);
-        const usaHistoricValues= await axios(usaHistoricUrl);
-        console.log("usaHistoricValues is: ", usaHistoricValues); 
 
-        const pastSevenDays = usaHistoricValues.data.slice(0,6);
-        const pastDay = usaHistoricValues.data.slice(0,1);
-        console.log("pastDay is: ", pastDay[0]); 
+        const usaHistoricValues = await axios(usaHistoricUrl);
+        const pastSevenDays = usaHistoricValues.data.slice(0, 6);
+        const pastDay = usaHistoricValues.data.slice(0, 1);
+        const formattedData = formatData(pastSevenDays);
 
-        const formattedData = formatData(pastSevenDays)
-        console.log("formattedData is: ", formattedData); 
-        // const a = formattedData.reverse()
-        // console.log("a is:", a) 
         setUsaHistoricData(formattedData.reverse());
-        setUsaCurrentData(pastDay[0]); 
-        // setStateHistoricalData(stateHistoricValues.data); 
-        // setStateCurrentData(stateCurrentValues.data)
-
+        setUsaCurrentData(pastDay[0]);
         setIsLoading(false);
+
       } catch (error) {
+
         setIsError(true);
+
       }
     };
- 
     fetchData();
   }, [usaHistoricUrl]);
- 
-  return [{  usaHistoricData, usaCurrentData, isLoading, isError }];
-}
 
-function Charts () {
+  return [{ usaHistoricData, usaCurrentData, isLoading }];
+};
 
-    // Set a hook here that fetches USDailyTotals
-    // Set another hook here that fetches some other data 
-    // https://www.robinwieruch.de/react-hooks-fetch-data
-    // https://github.com/muratkemaldar/using-react-hooks-with-d3/tree/09-racing-bar-chart/src
+function Charts() {
 
-  const [{ usaHistoricData, usaCurrentData, isLoading, isError }] = useFetchUSDailyTotals(); 
+  const [
+    { usaHistoricData, usaCurrentData, isLoading }
+  ] = useFetchUSDailyTotals();
 
-  const positiveTestChange = [{ values: usaHistoricData.map(({ positiveIncrease}) => positiveIncrease)}]; 
-  const deathsChange = [{ values: usaHistoricData.map(({ deathIncrease }) => deathIncrease)}];
-  const hospitalizedChange = [{ values: usaHistoricData.map(({ hospitalizedIncrease}) => hospitalizedIncrease)}];
-  const dates = usaHistoricData.map(({ abridgedDate}) => abridgedDate);
+  const positiveTestChange = [
+    { values: usaHistoricData.map(({ positiveIncrease }) => positiveIncrease) }
+  ];
 
-  const { death, hospitalizedCumulative, positive } = usaCurrentData; 
+  const deathsChange = [
+    { values: usaHistoricData.map(({ deathIncrease }) => deathIncrease) }
+  ];
 
-
-
-  console.log("positiveTestChange is: ", positiveTestChange); 
-
-  console.log("usaCurrentData is: ", usaCurrentData); 
-
+  const hospitalizedChange = [
+    {
+      values: usaHistoricData.map(
+        ({ hospitalizedIncrease }) => hospitalizedIncrease
+      )
+    }
+  ];
+  const dates = usaHistoricData.map(({ abridgedDate }) => abridgedDate);
+  const { death, hospitalizedCumulative, positive } = usaCurrentData;
 
   const testData = {
     labels: dates,
     datasets: positiveTestChange
-  }
+  };
 
   const deathData = {
     labels: dates,
     datasets: deathsChange
-  }
+  };
 
   const hospitalizedData = {
     labels: dates,
     datasets: hospitalizedChange
-  }
+  };
 
   const totalCasesData = {
-    title: "Total Cases", 
+    title: "Total Cases",
     total: positive
-  }
+  };
 
   const totalHospData = {
-    title: "Total Hospitalizations", 
+    title: "Total Hospitalizations",
     total: hospitalizedCumulative
-  }
+  };
 
   const totalDeathData = {
-    title: "Total Deaths", 
+    title: "Total Deaths",
     total: death
-  }
-
-  console.log("totalCasesData is: ", totalCasesData); 
-
-  // const newCase = usaHistoricData.filter(data => return )
-  // const newHospitalizations = 
-  // const newDeaths = 
-  // console.log("usaHistoricData[0] is: ", usaHistoricData); 
-
-  // Chart options
-    // https://frappe.io/charts/docs/basic/basic_chart
-    // https://www.reddit.com/r/reactjs/comments/9hnxee/which_chart_library_do_you_use_in_your_react_apps/
-    // http://recharts.org/en-US/examples/SimpleBarChart
-    // https://formidable.com/open-source/victory/gallery/alternative-events
-    // https://github.com/wingedeel/AK-react-d3-barchart/blob/master/src/components/App.js
-    // https://www.digitalocean.com/community/tutorials/7-ways-to-implement-conditional-rendering-in-react-applications
-    // Jan 14
-      // check that the useEffect is rendering after each await in the fetch (comment out the other awaits)
-      // check why the isLoading function ends in false 
+  };
 
   return (
-
     <div>
-      {console.log("isLoading is: ", isLoading)}
-      {!isLoading ?  
-      <Layout>
-      {/* <Container type="content"> */}
-      
-        <h1 className="data-summary text-center"> U.S. Summary </h1>
-        <ChartContainer className="random-class">
+      {!isLoading ? (
+        <Layout>
+          <h1 className="data-summary text-center"> U.S. Summary </h1>
+          <ChartContainer className="data-chart-container">
+            <div>
+              <DataCard
+                className="data-one text-center"
+                data={totalCasesData}
+              />
+            </div>
 
-          <div>
-            <DataCard className="data-one text-center" data={totalCasesData}/>
-          </div>
+            <div>
+              <DataCard className="data-two text-center" data={totalHospData} />
+            </div>
 
-          <div>
-            <DataCard className="data-two text-center" data={totalHospData}/>
-          </div>
+            <div>
+              <DataCard
+                className="data-three text-center"
+                data={totalDeathData}
+              />
+            </div>
 
-          <div>
-            <DataCard className="data-three text-center" data={totalDeathData}/>
-          </div>
+            <div className="chart-one text-center">
+              <h2 className="graph-title">Daily New Cases</h2>
+              <BarChart data={testData} />
+            </div>
 
-          <div className="chart-one text-center">
-            <h2 className="graph-title">Daily New Cases</h2>
-            <BarChart data={testData}/>
-          </div>
+            <div className="chart-two text-center">
+              <h2 className="graph-title">Daily New Hospitalizations</h2>
+              <BarChart data={deathData} />
+            </div>
 
-          <div className="chart-two text-center">
-            <h2 className="graph-title">Daily New Hospitalizations</h2>
-            <BarChart data={deathData}/>
-          </div>
-
-          <div className="chart-three text-center">
-            <h2 className="graph-title">Daily New Deaths</h2>
-            <BarChart data={hospitalizedData}/>
-          </div>
-          
-        </ChartContainer>
-      
-        {/* </Container> */}
-      </Layout>  
-      : <h1 className="text-center">Loading...</h1>}
-    {/* {usaHistoricData &&} */}
+            <div className="chart-three text-center">
+              <h2 className="graph-title">Daily New Deaths</h2>
+              <BarChart data={hospitalizedData} />
+            </div>
+          </ChartContainer>
+        </Layout>
+      ) : (
+          <h1 className="text-center">Loading...</h1>
+        )}
     </div>
-    
   );
-};
+}
 
 export default Charts;
 /* eslint-disable */
